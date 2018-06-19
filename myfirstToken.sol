2f35d8036079c9293e2486fbef91b6ff67a2cbd0
bhawanaSingh <bhawana.1441@gmail.com>
2018-06-19 16:01:26 +0530

    Update myfirstToken.sol

--- myfirstToken.sol
+++ myfirstToken.sol
@@ -3,22 +3,27 @@ pragma solidity ^0.4.11;
     contract myfirsttoken is ERC20 {
         string public constant symbol = "Bistack";
         string public constant name = "my first token";
-        uint public constant decimal = 18;
+        uint8  public constant decimal = 18;
         uint private constant totalsupply_of_token = 1000;
         
         mapping (address=>uint) private _balanceof;
         mapping (address => mapping (address=> uint)) private _allowance;
         
-        function Totalsupply_of_token() constant returns (uint _totalsupply) {
+        
+        function myfirsttok()public {
+            _balanceof[msg.sender] = totalsupply_of_token;
+        }
+        
+        function Totalsupply_of_token() public constant returns (uint _totalsupply) {
              _totalsupply = totalsupply_of_token;
             
         }
         
-        function balanceof(address _addr) constant returns (uint _balance){
+        function balanceof(address _addr) public constant returns (uint _balance){
             return _balanceof[_addr];
             
         }
-        function transfer(address _to , uint _value) returns (bool success) {
+        function transfer(address _to , uint _value) public returns (bool success) {
                if(_value > 0 && _value <=balanceof(msg.sender)) {
                    _balanceof[msg.sender] -= _value;    
                    _balanceof[_to] += _value;
@@ -27,15 +32,23 @@ pragma solidity ^0.4.11;
                    return false;
         }
         
-         function transfer_from( address _from ,address _to , uint _value) returns (bool success) {
-             
+         function transfer_from( address _from ,address _to , uint _value) public returns (bool success) {
+           if(_allowance[_from][msg.sender] > 0 && 
+           _value >0 &&
+           _allowance [_from][msg.sender] >= _value) {
+               _balanceof[_from] -= _value;
+               _balanceof[_to] += _value;
+               return true;
+               
+           }  
+           return false;
          }
         
-         function approve (address _spender, uint _value) returns (bool success) {
+         function approve (address _spender, uint _value)  public returns (bool success) {
              _allowance[msg.sender][_spender] = _value;
              return true;
          }
-         function allowance (address _owner, address _spender) constant returns (uint remaining) {
+         function allowance (address _owner, address _spender) public constant returns (uint remaining) {
                             return _allowance[_owner][_spender];
     
 
